53c04a8b94265edb5cc18aad46dac72574dc70a5
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-07 14:55:35 +0700

    Go operater
    Learning Go operater + - * /

--- mani.go
+++ mani.go
@@ -2,17 +2,14 @@ package main
 
 import "fmt"
 
-// นิยามค่าคงที่
 func main() {
-	var name string = "fah"
-	age := 21
-	var gender string = "M"
-	fmt.Printf("Type name is %T\n", name)
-	fmt.Printf("Value name is %v\n", name)
-	fmt.Println("")
-	fmt.Printf("Type age is %T\n", age)
-	fmt.Printf("Value age is %v\n", age)
-	fmt.Println("")
-	fmt.Printf("Type gender is %T\n", gender)
-	fmt.Printf("Value gender is %v\n", gender)
+	x := 1
+	z := 10
+	x++
+	fmt.Println("x + z:", x+z)
+	fmt.Println("x - z:", x-z)
+	fmt.Println("x * z:", x*z)
+	fmt.Println("x / z:", x/z)
+	fmt.Println("x mod z:", x%z)
+
 }
