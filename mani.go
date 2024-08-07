f75186040883f9fbf7f589fe936b966a7b183220
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-07 14:36:03 +0700

    Go formatting verbs

--- mani.go
+++ mani.go
@@ -7,5 +7,12 @@ func main() {
 	var name string = "fah"
 	age := 21
 	var gender string = "M"
-	fmt.Print("Nick name : ", name, "Age : ", age, "Gender : ", gender)
+	fmt.Printf("Type name is %T\n", name)
+	fmt.Printf("Value name is %v\n", name)
+	fmt.Println("")
+	fmt.Printf("Type age is %T\n", age)
+	fmt.Printf("Value age is %v\n", age)
+	fmt.Println("")
+	fmt.Printf("Type gender is %T\n", gender)
+	fmt.Printf("Value gender is %v\n", gender)
 }
