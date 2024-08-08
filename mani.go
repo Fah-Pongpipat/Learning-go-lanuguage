d720c6197b21369f29719f3b9b4c5a3c955588b6
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-08 15:14:10 +0700

    Go range loop

--- mani.go
+++ mani.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	list := []int{10, 20, 30, 40, 50}
+
+	for index, value := range list {
+		fmt.Println("Index :", index, " Value :", value)
+	}
+	fmt.Println("---------------------------")
 	for i := 0; i <= 10; i++ {
 		if i == 8 {
 			fmt.Println("Ended run code")
