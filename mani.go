40314121d2d30c4116c047e24f3f633389c5485f
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-08 15:07:31 +0700

    Go Break loop

--- mani.go
+++ mani.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 5; i++ {
-		if (i % 2) == 0 {
-			fmt.Println(i)
+	for i := 0; i <= 10; i++ {
+		if i == 8 {
+			fmt.Println("Ended run code")
+			break
+		} else {
+			fmt.Println("Round :", i)
 		}
 	}
 }
