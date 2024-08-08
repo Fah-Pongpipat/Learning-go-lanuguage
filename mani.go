0faca1578b401b85ac2695c6576a3c339e621b71
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-08 13:25:49 +0700

    Input data by user

--- mani.go
+++ mani.go
@@ -3,13 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	x := 1
-	z := 10
-	x++
-	fmt.Println("x + z:", x+z)
-	fmt.Println("x - z:", x-z)
-	fmt.Println("x * z:", x*z)
-	fmt.Println("x / z:", x/z)
-	fmt.Println("x mod z:", x%z)
-
+	var name string
+	fmt.Print("Enter your name: ")
+	fmt.Scanf("%s", &name)
+	fmt.Println("Your name is", name)
 }
