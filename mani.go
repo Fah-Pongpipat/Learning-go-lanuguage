6317c8991d6a45bdd3f11566797b24f8a2e9719f
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-08 15:15:33 +0700

    Go use Ignore

--- mani.go
+++ mani.go
@@ -5,16 +5,8 @@ import "fmt"
 func main() {
 	list := []int{10, 20, 30, 40, 50}
 
-	for index, value := range list {
-		fmt.Println("Index :", index, " Value :", value)
+	for _, value := range list {
+		fmt.Println(" Value :", value)
 	}
-	fmt.Println("---------------------------")
-	for i := 0; i <= 10; i++ {
-		if i == 8 {
-			fmt.Println("Ended run code")
-			break
-		} else {
-			fmt.Println("Round :", i)
-		}
-	}
+
 }
