03581e1c6af2e153a6690df15649430e8f18e567
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-08 15:40:16 +0700

    Go all function

--- mani.go
+++ mani.go
@@ -2,11 +2,29 @@ package main
 
 import "fmt"
 
+// call a function
+func myMessage() {
+	fmt.Println("I just got executed!")
+}
+
+// parameters
+func myName(name string) {
+	fmt.Println("Hello", name)
+}
+
+// function return and input parameters
+func plus(a int, b int) int {
+	return a + b
+}
+
+// function return
+func printHello() string {
+	return "Hello  world"
+}
 func main() {
-	list := []int{10, 20, 30, 40, 50}
-
-	for _, value := range list {
-		fmt.Println(" Value :", value)
-	}
-
+	myMessage()
+	myName("Fah")
+	sum := plus(5, 6)
+	fmt.Println(sum)
+	fmt.Println(printHello())
 }
