ff188e266349d4cf92bc4ee7e93757f54584cfe2
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-08-08 16:18:16 +0700

    Go Create package and finished study

--- mani.go
+++ mani.go
@@ -1,20 +1,10 @@
 package main
 
-import "fmt"
-
-type Person struct {
-	name   string
-	age    int
-	gender string
-}
+import (
+	"fmt"
+	"studying-go/calculator"
+)
 
 func main() {
-	var person1 Person
-	person1.name = "Fah"
-	person1.age = 21
-	person1.gender = "M"
-	fmt.Println(person1)
-
-	person2 := Person{name: "Ed", age: 12, gender: "M"}
-	fmt.Println(person2)
+	fmt.Println(calculator.Add(100, 6))
 }
